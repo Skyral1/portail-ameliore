d7004b0dc1ae3f4d0f94482b60fd4fb1f4778e45
Thibault Soquette <thibault.soquette.pro@gmail.com>
2025-10-04 16:57:50 +0200

    Refactor new-site.php for improved structure and readability; add maxlength attributes to form inputs
    Signed-off-by: Thibault Soquette <thibault.soquette.pro@gmail.com>

--- php/new-site.php
+++ php/new-site.php
@@ -1,41 +1,42 @@
 <?php
 session_start();
-
-// Vérifier si l'utilisateur est connecté
 if (!isset($_SESSION['username'])) {
     header('Location: login.php');
+    exit;
 }
-
-// Afficher un message de bienvenue
-// echo 'Bienvenue, ' . $_SESSION['username'] . '!';
 ?>
-
 <!DOCTYPE html>
-<html>
-
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
     <title>Ajouter un site</title>
     <link rel="shortcut icon" href="../assets/img/logo_rounded.png" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="../assets/css/new-site.css">
 </head>
-
 <body>
     <h1>Ajouter un site</h1>
-    <form method="post" action="./traitement.php">
+    <?php
+    // Affichage message succès/erreur via GET si besoin
+    if (isset($_GET['message'])) {
+        echo '<p>' . htmlspecialchars($_GET['message']) . '</p>';
+    }
+    ?>
+    <form method="post" action="traitement.php">
         <label for="titre">Titre :</label>
-        <input type="text" id="titre" name="titre" required>
-        <br>
+        <input type="text" id="titre" name="titre" required maxlength="255">
+
         <label for="image">Lien de l'image :</label>
-        <input type="text" id="image" name="image" required>
-        <br>
+        <input type="url" id="image" name="image" required maxlength="255" placeholder="https://exemple.com/image.png">
+
         <label for="description">Description :</label>
-        <textarea id="description" name="description" required></textarea>
-        <br>
+        <textarea id="description" name="description" required maxlength="800"></textarea>
+
         <label for="url">URL :</label>
-        <input type="text" id="url" name="url" required>
-        <br>
+        <input type="url" id="url" name="url" required maxlength="255" placeholder="https://exemple.com/">
+
         <input type="submit" value="Ajouter">
     </form>
+    <br>
+    <a class="button button_return" href="index.php">Retour</a>
 </body>
-
 </html>
